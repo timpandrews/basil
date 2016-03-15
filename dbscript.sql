3b9876d57660cb5ee0b363c059301675a621f80a
Tim Andrews <tim.andrews@imagineifinc.com>
2016-03-15 11:37:02 -0400

    Added flask-migrate

--- dbscript.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user
-(
-   id          int(10),
-   fullname    varchar(80),
-   email       varchar(80),
-   username    varchar(80),
-   password    varchar(80),
-   is_author   tinyint(1) DEFAULT 0
-)
---
+++ user/models.py
@@ -0,0 +1,19 @@
+from basil import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(80))
+    email = db.Column(db.String(35), unique=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(60))
+    is_admin = db.Column(db.Boolean)
+
+    def __init__(self, fullname, email, username, password, is_admin=False):
+        self.fullname = fullname
+        self.email = email
+        self.username = username
+        self.password = password
+        self.is_admin = is_admin
+
+    def __repr__(self):
+        return '<Author %r>' % self.username
