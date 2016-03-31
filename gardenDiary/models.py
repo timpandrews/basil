335fec3bd2ca1fd74727e26693f2493d8d2bcd7f
Tim Andrews <tim.andrews@imagineifinc.com>
2016-03-31 12:20:54 -0400

    Added reminder model & updated db

--- gardenDiary/models.py
+++ gardenDiary/models.py
@@ -33,3 +33,35 @@ class Diary(db.Model):
 
     def __repr__(self):
         return '<diaryEntry %r>' % self.title
+
+class Reminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(80))
+    detail = db.Column(db.Text)
+    badge = db.Column(db.String(256))
+    create_date = db.Column(db.DateTime)
+    update_date = db.Column(db.DateTime)
+    active = db.Column(db.Boolean)
+
+    @property
+    def imgsrc(self):
+        return uploaded_images.url(self.badge)
+
+    def __init__(self, user, title, detail, badge=None, create_date=None, update_date=None, active=True):
+        self.user_id = user.id
+        self.title = title
+        self.detail = detail
+        self.badge = badge
+        if create_date is None:
+            self.create_date = datetime.utcnow()
+        else:
+            self.publish_date = publish_date
+        if update_date is None:
+            self.update_date = datetime.utcnow()
+        else:
+            self.update_date = update_date
+        self.active = active
+
+    def __repr__(self):
+        return '<reminder %r>' % self.title
